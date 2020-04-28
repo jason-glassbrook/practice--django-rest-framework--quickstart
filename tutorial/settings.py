8af4bfda9579461dba58eb25a23916efff9ca318
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-04-28 12:00:13 -0400

    add rest_framework

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -70,6 +71,11 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "tutorial.wsgi.application"
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
